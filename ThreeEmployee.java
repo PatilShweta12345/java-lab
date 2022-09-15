819c9068cd4bab3b383364776e3c5c4723fa424f
PatilShweta12345 <113102537+PatilShweta12345@users.noreply.github.com>
2022-09-15 18:00:24 +0530

    Add files via upload

---
+++ ThreeEmployee.java
@@ -0,0 +1,11 @@
+class ThreeEmployee
+{
+    static public void main(String args[])
+	{
+	    int a=10000;
+		int b=11000;
+		int c=5000;
+		int z=a-(b+c);
+		System.out.print("Salary of the third employee" +z);
+    }
+}
