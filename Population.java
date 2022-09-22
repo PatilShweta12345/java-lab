8539fc34c1fe014cea23f1da78151b61f3e176ab
PatilShweta12345 <113102537+PatilShweta12345@users.noreply.github.com>
2022-09-22 18:05:28 +0530

    Add files via upload

---
+++ Population.java
@@ -0,0 +1,18 @@
+package com.OOBs;
+
+public class Population {
+
+	public static void main(String[] args)
+	{                   
+	
+    int a=175000;
+    int b=262500;
+    int c=87500;
+    int d=50;
+    int e=d/10;
+    System.out.println("Increased Population in 10 years :-"  +c);
+    System.out.println("Increased population in 10 years :-"  +d+ "%");
+    System.out.println("increased population for 1 year is  :-"  +e+ "%");
+    
+    }
+}
